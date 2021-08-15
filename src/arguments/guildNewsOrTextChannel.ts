a00f94f2eff141b74886cbda2167328fb79f55a2
Chiitoi <vrajakul@gmail.com>
2021-08-15 15:46:39 -0400

    fix(arguments): change types

--- src/arguments/guildNewsOrTextChannel.ts
+++ src/arguments/guildNewsOrTextChannel.ts
@@ -1,17 +1,18 @@
 import { ApplyOptions } from '@sapphire/decorators'
+import { GuildBasedChannelTypes } from '@sapphire/discord.js-utilities'
 import { ExtendedArgument } from '@sapphire/framework'
 import type { ExtendedArgumentContext, ExtendedArgumentOptions } from '@sapphire/framework'
-import type { GuildChannel, NewsChannel, TextChannel } from 'discord.js'
+import type { NewsChannel, TextChannel } from 'discord.js'
 
 @ApplyOptions<ExtendedArgumentOptions<'guildChannel'>>({ baseArgument: 'guildChannel' })
 export class SakuraArgument extends ExtendedArgument<'guildChannel', NewsChannel | TextChannel> {
-    public handle(channel: GuildChannel, context: ExtendedArgumentContext) {
+    public handle(channel: GuildBasedChannelTypes, context: ExtendedArgumentContext) {
         return this.isNewsOrTextChannel(channel)
             ? this.ok(channel)
             : this.error({ context, message: 'No announcement or text channel found.', parameter: context.parameter })
     }
 
-    private isNewsOrTextChannel(channel: GuildChannel): channel is NewsChannel | TextChannel {
+    private isNewsOrTextChannel(channel: GuildBasedChannelTypes): channel is NewsChannel | TextChannel {
         return ['GUILD_NEWS', 'GUILD_TEXT'].includes(channel.type)
     }
 }
