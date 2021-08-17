518c6982cc91e39f0408471591312c9836bdb5b8
Chiitoi <vrajakul@gmail.com>
2021-08-17 00:52:21 -0400

    feat(commands): (for "counts" command) display total channel counts + minor refactor

--- src/commands/admin/counts.ts
+++ src/commands/admin/counts.ts
@@ -4,7 +4,7 @@ import { replyWithSelectPages } from '#utils'
 import { ApplyOptions } from '@sapphire/decorators'
 import type { Args } from '@sapphire/framework'
 import { container } from '@sapphire/pieces'
-import type { CategoryChannel, Guild, GuildChannel, Message, MessageEmbed, ThreadChannel } from 'discord.js'
+import type { CategoryChannel, Collection, Guild, GuildChannel, Message, MessageEmbed, ThreadChannel } from 'discord.js'
 
 @ApplyOptions<SakuraCommandOptions>({
     description: 'Displays a server\'s channel counts (by category)',
@@ -20,32 +20,44 @@ export class CountsCommand extends SakuraCommand {
     }
 
     private getCountsEmbed(guild: Guild): Partial<MessageEmbed> {
-        const guildId = BigInt(guild.id)
-        const { categoryIds, ignoreIds, infoEmbedColor } = container.settings.get(guildId)
-        const embed: Partial<MessageEmbed> = { color: infoEmbedColor, title: `Channel counts for "${ guild.name }"` }
+        const chosenGuildId = BigInt(guild.id)
+        const chosenGuildCategoryDescriptions: string[] = []
+        const embed: Partial<MessageEmbed> = { color: container.settings.getInfoEmbedColor(chosenGuildId), title: `Channel counts for "${ guild.name }"` }        
+        let totalIgnoreCount = 0, totalNewsCount = 0, totalTextCount = 0
+        
+        for (const guild of container.client.guilds.cache.values()) {
+            const guildId = BigInt(guild.id)
+            const { categoryIds, ignoreIds } = container.settings.get(guildId)
 
-        if (!categoryIds.length)
-            embed.description = 'No added categories in this server'
-        else {
-            const guildChannels = guild.channels.cache
-            const isCheckCategoryChannel = (channel: GuildChannel | ThreadChannel): channel is CategoryChannel => categoryIds.includes(BigInt(channel.id)) && channel.type === 'GUILD_CATEGORY'
-            const checkCategoryChannels = guildChannels.filter(isCheckCategoryChannel).sort((c1, c2) => c1.position - c2.position)
-            
-            embed.description = checkCategoryChannels.map(({ children, name }) => {
-                let ignoreCount = 0, newsCount = 0, textCount = 0
-    
+            if (!categoryIds.length)
+                continue
+
+            const guildCategoryChannels = guild.channels.cache.filter(channel => categoryIds.includes(BigInt(channel.id)) && channel.type === 'GUILD_CATEGORY') as Collection<string, CategoryChannel>
+            let categoryIgnoreCount = 0, categoryNewsCount = 0, categoryTextCount = 0
+
+            for (const { children, name } of guildCategoryChannels.values()) {
                 for (const { id, type } of children.values()) {
                     if (ignoreIds.includes(BigInt(id)))
-                        ignoreCount++
+                        categoryIgnoreCount++
                     if (type === 'GUILD_NEWS')
-                        newsCount++
+                        categoryNewsCount++
                     if (type === 'GUILD_TEXT')
-                        textCount++
+                        categoryTextCount++
                 }
-    
-                return `**Category: ** ${ name }\n**Count: ${ newsCount + textCount - ignoreCount }** channels (**${ newsCount }** announcement and **${ textCount }** text), with **${ ignoreCount }** ignored`
-            }).join('\n')
+
+                totalIgnoreCount += categoryIgnoreCount
+                totalNewsCount += categoryNewsCount
+                totalTextCount += categoryTextCount
+
+                if (guildId === chosenGuildId)
+                    chosenGuildCategoryDescriptions.push(`**Category:** ${ name }\n**Count: ${ categoryNewsCount + categoryTextCount - categoryIgnoreCount }** channels (**${ categoryNewsCount }** announcement and **${ categoryTextCount }** text), with **${ categoryIgnoreCount }** ignored\n`)
+            }              
         }
+
+        embed.description = chosenGuildCategoryDescriptions.length
+            ? chosenGuildCategoryDescriptions.join('\n')
+            : 'No added categories in this server.'
+        embed.footer = { text: `${ totalNewsCount + totalTextCount - totalIgnoreCount } total channels (${ totalNewsCount } announcement and ${ totalTextCount } text channels, with ${ totalIgnoreCount } ignored)` }       
 
         return embed
     }
